a156a5b41489a984b1b3be3afd9500716242fe79
vishal <vishalfullstack1@gmail.com>
2025-05-30 17:59:14 +0530

    Add files via upload

---
+++ Project - 2 v-foundation clone/index.js
@@ -0,0 +1,13 @@
+document.querySelector(".btn").addEventListener("click",() => {
+ alert("page is loading! please Wait") 
+})
+
+document.querySelector(".btn").addEventListener("contextmenu",() => {
+ alert("Do not right click please! Wait") 
+})
+
+document.querySelector(".sec-btn").addEventListener("click",() => {
+ alert("page is loading! please Wait") 
+})
+
+ 
